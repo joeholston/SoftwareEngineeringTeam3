66b03a07020bdb6d5f6a50b8c36a9c7c7c3ab88c
BeilerSA1 <BeilerSA1@gcc.edu>
2018-03-14 11:39:14 -0400

    Made creating DB it's own class

---
+++ SoftwareEngineering/SoftwareEngineering/BuildDB.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SoftwareEngineering
+{
+    class BuildDB
+    {
+        public List<Course> buildCourseList()
+        {
+            List<Course> courseDB = new List<Course>();
+            using (StreamReader reader = new StreamReader("CourseDB_WithFictionalCapacities.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    string[] values = line.Split(',');
+                    courseDB.Add(new Course(values[0], values[1], values[2], Convert.ToDateTime(values[3]), Convert.ToDateTime(values[4]), values[5], values[6], values[7], int.Parse(values[8]), int.Parse(values[9])));
+                }
+            }
+            return courseDB;
+        }
+    }
+}
