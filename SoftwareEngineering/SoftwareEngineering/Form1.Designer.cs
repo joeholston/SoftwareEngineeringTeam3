e0eb397be0c230662c5b9ce441abafadd8d457c8
Joe Holston <holstonjx1@gcc.edu>
2018-03-18 17:57:10 -0400

    Search UI

--- SoftwareEngineering/SoftwareEngineering/Form1.Designer.cs
+++ SoftwareEngineering/SoftwareEngineering/Form1.Designer.cs
@@ -28,20 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.searchDropDown = new System.Windows.Forms.ComboBox();
+            this.searchGroup = new System.Windows.Forms.GroupBox();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.searchGroup.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // searchBox
+            // 
+            this.searchBox.Location = new System.Drawing.Point(154, 31);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(235, 20);
+            this.searchBox.TabIndex = 0;
+            // 
+            // searchDropDown
+            // 
+            this.searchDropDown.FormattingEnabled = true;
+            this.searchDropDown.Items.AddRange(new object[] {
+            "Course Code"});
+            this.searchDropDown.Location = new System.Drawing.Point(12, 30);
+            this.searchDropDown.Name = "searchDropDown";
+            this.searchDropDown.Size = new System.Drawing.Size(121, 21);
+            this.searchDropDown.TabIndex = 1;
+            // 
+            // searchGroup
+            // 
+            this.searchGroup.Controls.Add(this.dateTimePicker);
+            this.searchGroup.Controls.Add(this.searchBox);
+            this.searchGroup.Controls.Add(this.searchDropDown);
+            this.searchGroup.Location = new System.Drawing.Point(12, 12);
+            this.searchGroup.Name = "searchGroup";
+            this.searchGroup.Size = new System.Drawing.Size(408, 111);
+            this.searchGroup.TabIndex = 2;
+            this.searchGroup.TabStop = false;
+            this.searchGroup.Text = "Search";
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(12, 70);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(197, 20);
+            this.dateTimePicker.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(708, 440);
+            this.ClientSize = new System.Drawing.Size(954, 516);
+            this.Controls.Add(this.searchGroup);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.searchGroup.ResumeLayout(false);
+            this.searchGroup.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.ComboBox searchDropDown;
+        private System.Windows.Forms.GroupBox searchGroup;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
     }
 }
 
